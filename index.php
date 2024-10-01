f1c3aa66d7e16d95687f6d9b0a6f55fdd52b5216
MauricioVargas <mv5284636@gmail.com>
2024-10-01 10:35:03 -0400

    Conexión a base de datos y manejo de rutas

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 ////////**controladores *///////
 require_once "controlador/rutas.controlador.php";
+require_once "controlador/cursos.controlador.php";
+/////////**modelos *///////////
+require_once "modelo/cursos.modelo.php";
 $rutas=new ControladorRutas();
 $rutas->inicio();
 ?>
--- rutas/rutas.php
+++ rutas/rutas.php
@@ -1,22 +1,23 @@
 <?php
-
+/* capturamos la ruta */
 $array=explode("/",$_SERVER["REQUEST_URI"]);
-
+/* cuando no se hace ninguna peticion a la API */
 if(count(array_filter($array))==1){
     $json=array(
         "detalle"=>"Sin solicitudes",
     );
 echo json_encode($json, true);
 }else{
-
+/* cuando si se hace una peticion a la API */
 if(count(array_filter($array))==2){
-    $json=array(
-        "detalle"=>"¿Cuál es la solicitud?",
-    );
-echo json_encode($json, true);
-
+    /*cursos*/
+if(array_filter($array)[2]=="cursos"){
+    /*ver listas de cursos* */
+    if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"]=="GET"){
+        $cursos=new ControladorCursos();
+        $cursos->index();
+    }
 }
-
-
+}
 }
 ?>
